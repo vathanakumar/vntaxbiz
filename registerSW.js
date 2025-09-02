f3a848bbb8f4daf522ec27631beaa6f0b49e2dba
Mirunan <mirunansjcr@gmail.com>
2025-09-02 13:21:10 -0400

    .

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/vntaxbiz/vntaxbiz.webmanifest', { scope: '/vntaxbiz/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/vntaxbiz/manifest.webmanifest', { scope: '/vntaxbiz/' })})}
